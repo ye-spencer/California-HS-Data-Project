fff6645e7e08d4c951d45bc7c4f84460a4266f1f
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2024-08-05 11:30:23 +0800

    save score as float not string

--- Queries/filtering.sql
+++ Queries/filtering.sql
@@ -3,8 +3,8 @@ SELECT
   `County Code`,
   `District Code`,
   `Students Tested`,
-  (10 * `Percentage Standard Exceeded` + 7 * `Percentage Standard Met` + 2 * `Percentage Standard Nearly Met` + 0.5 * `Percentage Standard Not Met`) AS score
-FROM (
+  CAST(10 * `Percentage Standard Exceeded` + 7 * `Percentage Standard Met` + 2 * `Percentage Standard Nearly Met` + 0.5 * `Percentage Standard Not Met` AS FLOAT64) AS score
+FROM ( -- Subquery to combine school's various scores across test versions
 SELECT 
   `School Code`, 
   `County Code`,
@@ -16,11 +16,13 @@ SELECT
   AVG(CAST(`Percentage Standard Nearly Met` AS FLOAT64)) AS `Percentage Standard Nearly Met`,
   AVG(CAST(`Percentage Standard Not Met` AS FLOAT64)) AS `Percentage Standard Not Met`
 FROM `california-hs-project.Main.Testing Data`
-WHERE grade = 11
+WHERE grade = 11 -- Only get the scores for individual schools, no aggregated scores or missing scores
   AND (NOT `School Code` = 0)
   AND (NOT `Students Tested` = "*")
   AND (NOT `Percentage Standard Exceeded` = "*")
 GROUP BY `School Code`, `County Code`, `District Code`;
 ) subquery
+
+-- Need to run a loop to see which power of n (school size) we should divide the schools by to minimize school size as a factor
   
 
